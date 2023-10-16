0414a050bc6868c2afef86790e8e5d7f83649669
Prasdimas <prasdimasetyo@gmail.com>
2023-10-17 01:15:25 +0700

    deploy vercel

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ try {
     console.error(error);
 }
 
-app.use(cors({ credentials:true, origin:'https://chat-katalismedia.com' }));
+app.use(cors({ credentials:true, origin:'https://chat.katalismedia.com' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
